80c071c40d42b4f04f9071f72bdb779322f31071
Alexander Brandstedt <alexander.brandstedt@netent.com>
2020-04-28 12:03:41 +0200

    update to use local copy of package

--- main.go
+++ main.go
@@ -7,11 +7,16 @@ import (
 	"os"
 
 	"github.com/google/go-github/v31/github"
+	gsw "github.com/shelmangroup/github-secrets-sync/pkg"
 	"golang.org/x/oauth2"
 )
 
 var client *github.Client
 var ctx = context.Background()
+var owner = "shelmangroup"
+var repo = "github-secrets-sync"
+var secretName = "TEST"
+var secretValue = []byte("super secret value")
 
 func main() {
 	fmt.Println("vim-go")
@@ -25,10 +30,19 @@ func main() {
 	tc := oauth2.NewClient(ctx, ts)
 	client = github.NewClient(tc)
 
-	secret, _, err := client.Actions.GetSecret(ctx, "mad01", "dummy", "FOO")
+	secret, _, err := client.Actions.GetSecret(ctx, owner, repo, secretName)
 	if err != nil {
 		log.Printf("Ops.. %s\n", err.Error())
 	} else {
 		log.Printf("secret: %+v", secret)
 	}
+
+	writer := gsw.NewSecretWriter(token)
+	status, err := writer.Write(owner, repo, secretName, secretValue)
+	if err != nil {
+		log.Printf("Ops.. %s\n", err.Error())
+	} else {
+		log.Printf("secret write status: %s\n", status)
+	}
+
 }
