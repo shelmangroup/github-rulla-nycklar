7be8f8284484bd905197a80c5d4039e3110aa80d
Alexander Brandstedt <alexander.brandstedt@netent.com>
2020-04-28 15:12:35 +0200

    account struct and funcs to support rotation of keys

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"os"
 
@@ -15,28 +14,81 @@ var owner = "shelmangroup"
 var repo = "github-secrets-sync"
 var secretName = "TEST"
 var secretValue = []byte("super secret value")
+var testEmail = "github-test@xXxXx.iam.gserviceaccount.com"
+var testProject = "xXxXx"
 
-// createKey creates a service account key.
-func rotateServiceAccountKey(w io.Writer, serviceAccountEmail string) (*iam.ServiceAccountKey, error) {
+type IamServiceAccountClient struct {
+	service *iam.Service
+}
+
+func NewIamClient() *IamServiceAccountClient {
 	ctx := context.Background()
 	service, err := iam.NewService(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("iam.NewService: %v", err)
+		log.Fatalf("iam.NewService: %v", err)
 	}
+	return &IamServiceAccountClient{service: service}
+}
 
+// createKey creates a service account key.
+func (i *IamServiceAccountClient) createKey(serviceAccountEmail string) (*iam.ServiceAccountKey, error) {
 	resource := "projects/-/serviceAccounts/" + serviceAccountEmail
 	request := &iam.CreateServiceAccountKeyRequest{}
-	key, err := service.Projects.ServiceAccounts.Keys.Create(resource, request).Do()
+	key, err := i.service.Projects.ServiceAccounts.Keys.Create(resource, request).Do()
 	if err != nil {
 		return nil, fmt.Errorf("Projects.ServiceAccounts.Keys.Create: %v", err)
 	}
-	fmt.Fprintf(w, "Created key: %v", key.Name)
+	log.Printf("Created key: %v", key.Name)
 	return key, nil
 }
 
-func main() {
-	fmt.Println("vim-go")
+// deleteKey deletes a service account key.
+func (i *IamServiceAccountClient) deleteKey(fullKeyName string) error {
+	var err error
+	_, err = i.service.Projects.ServiceAccounts.Keys.Delete(fullKeyName).Do()
+	if err != nil {
+		return fmt.Errorf("Projects.ServiceAccounts.Keys.Delete: %v", err)
+	}
+	log.Printf("Deleted key: %v", fullKeyName)
+	return nil
+}
 
+// listKey lists a service account's keys.
+func (i *IamServiceAccountClient) listKeys(serviceAccountEmail string) ([]*iam.ServiceAccountKey, error) {
+	resource := "projects/-/serviceAccounts/" + serviceAccountEmail
+	response, err := i.service.Projects.ServiceAccounts.Keys.List(resource).Do()
+	if err != nil {
+		return nil, fmt.Errorf("Projects.ServiceAccounts.Keys.List: %v", err)
+	}
+	for _, key := range response.Keys {
+		log.Printf("Listing key: %v", key.Name)
+	}
+	return response.Keys, nil
+}
+
+// rotateKey, and remove old keys if exists
+func (i *IamServiceAccountClient) rotateKey(serviceAccountEmail string) (*iam.ServiceAccountKey, error) {
+	keys, err := i.listKeys(serviceAccountEmail)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, key := range keys {
+		err = i.deleteKey(key.Name)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	key, err := i.createKey(serviceAccountEmail)
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+
+}
+
+func main() {
 	token := os.Getenv("GITHUB_AUTH_TOKEN")
 	if token == "" {
 		log.Fatal("Unauthorized: No token present")
